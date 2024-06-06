e5efa3426f45aade558932b5f79e126608607980
Johnchukwu <ifeanyichukwujohn23@gmail.com>
2024-06-06 17:52:27 +0100

    Popularcollection section page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
-export default function App() {
+
+import './App.css';
+import PopularCollection from './components/product/PopularCollection';
+
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="App">
+      <header className="App-header">
+        <h1>My E-Commerce Site</h1>
+      </header>
+      <main>
+        <PopularCollection />
+      </main>
+    </div>
+  );
 }
+
+export default App;
