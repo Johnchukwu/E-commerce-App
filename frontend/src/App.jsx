ad800029f62c65b9a7b16768ed1bee95d46d1885
twilight05 <54307284+twilight05@users.noreply.github.com>
2024-06-07 15:28:55 +0100

    Added Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
-export default function App() {
+import React from 'react'
+import Footer from './components/Footer'
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Footer/>
   )
 }
+
+export default App
