058041ec6a333fe370693ee64e4c02ee2f21c9ca
sodiqganiu <sodiqganiu412@gmail.com>
2024-06-10 13:13:03 +0100

    new commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
-import Hero from './components/Hero'
-import PopularCollection from './components/product/PopularCollection'
-import Header from './components/Header'
+
 import About from './components/About'
 
 const App = () => {
   return (
     <>
-    <div>
-      {/* <Hero />
-      <PopularCollection />
-      <Header/> */}
-      
-
-      
-    </div>
-
     <About/>
     </>
   )
